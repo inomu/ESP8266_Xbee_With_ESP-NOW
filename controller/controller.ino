2a13a92aa5177df6409f650d6e916ef62de76426
inomu <main.it07@gmail.com>
2017-05-02 04:46:05 +0900

    refactoring

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,4 +1,5 @@
 //Controller
+//ESPのRXDとXbeeの３ピン（DIN）をつなぐと何故か動く．
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 extern "C" {
@@ -8,38 +9,16 @@ extern "C" {
 
 #define WIFI_DEFAULT_CHANNEL 1
 
-//SlaveのSOFTAP_IFのMACアドレスを
+//SlaveのSOFTAP_IFのMACアドレスを書く
 uint8_t mac[] = {0x5E,0xCF,0x7F,0x85,0x07,0xBE};
 
-uint8_t message[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x08 };
-
-void printMacAddress(uint8_t* macaddr) {
-	Serial.print("{");
-	for (int i = 0; i < 6; i++) {
-		Serial.print("0x");
-		Serial.print(macaddr[i], HEX);
-		if (i < 5) Serial.print(',');
-	}
-	Serial.println("}");
-
-}
-
 void setup() {
-  //This pilot lamp expressing activation.
-	//pinMode(13, OUTPUT);
-
+	//シリアル通信の速度変更はここを書き換える．
 	Serial.begin(19200);
-	Serial.println("Initializing...");
 	WiFi.mode(WIFI_STA);
 
-	uint8_t macaddr[6];
-	wifi_get_macaddr(STATION_IF, macaddr);
-	Serial.print("mac address (STATION_IF): ");
-	printMacAddress(macaddr);
-
-	wifi_get_macaddr(SOFTAP_IF, macaddr);
-	Serial.print("mac address (SOFTAP_IF): ");
-	printMacAddress(macaddr);
+  //This pilot lamp expressing activation.
+	//pinMode(13, OUTPUT);
 
 	if (esp_now_init()==0) {
 		//Serial.println("direct link  init ok");
@@ -50,47 +29,16 @@ void setup() {
 	}
 
 	esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-
 	esp_now_register_recv_cb([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
-		Serial.println("recv_cb");
-
-		Serial.print("mac address: ");
-		printMacAddress(macaddr);
-
-		Serial.print("data: ");
-		for (int i = 0; i < len; i++) {
-			Serial.print(data[i], HEX);
-		}
-		Serial.println("");
 	});
 
 	esp_now_register_send_cb([](uint8_t* macaddr, uint8_t status) {
-		//Serial.print("send_cb:");
-    //Serial.print("data: ");
-
-		/*
-    for (int i = 0; i < sizeof(message); i++){
-      Serial.print(" 0x");
-      Serial.print(message[i],HEX);
-    }
-		*/
-    //Serial.println("");
-
-		//Serial.print("mac address: ");
-		//printMacAddress(macaddr);
-
-		//Serial.print("status = "); Serial.println(status);
-
-
 	});
+
+	//uint8_t message[] = { 0x42};
   //esp_now_send(mac, message, sizeof(message));
 
-	uint8_t message[] = { 0x42};
-  esp_now_send(mac, message, sizeof(message));
-
 	int res = esp_now_add_peer(mac, (uint8_t)ESP_NOW_ROLE_SLAVE,(uint8_t)WIFI_DEFAULT_CHANNEL, NULL, 0);
-//	esp_now_unregister_recv_cb();
-//	esp_now_deinit();
 }
 
 void loop() {
@@ -98,7 +46,6 @@ void loop() {
   if(Serial.available() > 0){
     int input = Serial.read();
     uint8_t rcv_data = input;
-		//Serial.println(rcv_data);
     esp_now_send(mac, &rcv_data, sizeof(rcv_data));
   }
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -11,34 +11,16 @@ extern "C" {
 //ControllerのSTATION_IFのMACアドレスを書く
 uint8_t mac[] = {0x5C,0xCF,0x7F,0x84,0xD5,0x18};
 
-void printMacAddress(uint8_t* macaddr) {
-  Serial.print("{");
-  for (int i = 0; i < 6; i++) {
-    Serial.print("0x");
-    Serial.print(macaddr[i], HEX);
-    if (i < 5) Serial.print(',');
-  }
-  Serial.println("}");
-}
-
 void setup() {
+  //シリアル通信の速度変更はここを書き換える．
+  Serial.begin(19200);
+  delay(10);
+
   //This pilot lamp expressing activation.
   //pinMode(13, OUTPUT);
 
-  Serial.begin(19200);
-  //Serial.println("Initializing...");
-
   WiFi.mode(WIFI_AP);
   WiFi.softAP("foobar", "12345678", 1, 0);
-
-  uint8_t macaddr[6];
-  //wifi_get_macaddr(STATION_IF, macaddr);
-  //Serial.print("mac address (STATION_IF): ");
-  //printMacAddress(macaddr);
-
-  wifi_get_macaddr(SOFTAP_IF, macaddr);
-  //Serial.print("mac address (SOFTAP_IF): ");
-  printMacAddress(macaddr);
 
   if (esp_now_init() == 0) {
     //Serial.println("init");
@@ -51,28 +33,15 @@ void setup() {
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
 
   esp_now_register_recv_cb([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
-    //Serial.println("recv_cb");
-    //Serial.print("mac address: ");
-    //Serial.print("data: ");
     for (int i = 0; i < len; i++) {
-      //Serial.print(" 0x");
-      //Serial.print(data[i],  HEX);
       Serial.write(data[i]);
-
     }
-    //Serial.println("");
   });
 
   esp_now_register_send_cb([](uint8_t* macaddr, uint8_t status) {
-    //printMacAddress(macaddr);
-
-    //Serial.print("status = "); Serial.println(status);
   });
 
   int res = esp_now_add_peer(mac, (uint8_t)ESP_NOW_ROLE_CONTROLLER,(uint8_t)WIFI_DEFAULT_CHANNEL, NULL, 0);
-
-//  esp_now_unregister_recv_cb();
-//  esp_now_deinit();
 }
 
 void loop() {
